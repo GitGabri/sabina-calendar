2536dc212bc08f4fdff6ca43688f52b16caed7e1
Gabriele Motta <galimero95@hotmail.com>
2023-12-31 12:22:55 +1000

    first commit with app src folder

---
+++ src/components/calendar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type CalendarProps = {
+
+}
+
+export default function Calendar() {
+
+    return(
+        <div className='calendar-container'>
+            helloworld
+        </div>
+    );
+}
